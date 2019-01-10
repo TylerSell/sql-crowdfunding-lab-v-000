18038eb65e655d75b68584aa189b6d0c3e1b697a
tylerselldesign <tylerselldesign@gmail.com>
2019-01-10 05:35:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,12 +23,12 @@ GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
-FROM projects, pledges
-WHERE pledges.project_id = projects.id
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS over 
+FROM projects
+JOIN pledges
+ON projects.id = pledges.user_id 
 GROUP BY projects.title
-HAVING projects.id = pledges.project_id
-ORDER BY SUM(pledges.amount), projects.funding_goal > -1 DESC;"
+HAVING over >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
