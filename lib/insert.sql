b9a880e18633eebf903278e17f8149599b134319
tylerselldesign <tylerselldesign@gmail.com>
2019-01-10 04:45:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,21 @@
 INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 56);
+
