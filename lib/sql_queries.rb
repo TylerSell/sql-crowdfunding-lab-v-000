b80da6f5e99c6df4dffdaf320a521b003533b5f0
tylerselldesign <tylerselldesign@gmail.com>
2019-01-10 05:10:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,11 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT projects.title, SUM(pledges.amount)
-FROM projects
-LEFT OUTER JOIN pledges
-
-"
+"SELECT Projects.title, SUM(amount)
+FROM projects, pledges
+GROUP BY pledges.project_id, projects.title 
+HAVING projects.id = pledges.project_id 
+ORDER BY projects.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
@@ -33,3 +33,7 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "Write your SQL query Here"
 end
+# def selects_the_titles_of_all_projects_and_their_pledge_amounts
+# "SELECT Projects.title, SUM(amount) FROM projects, pledges GROUP BY pledges.project_id, 
+# Projects.title HAVING Projects.id = pledges.project_id ORDER BY projects.title ASC;" 
+# end
