aabe70f40a830834167524eff9195a18d36f534d
tylerselldesign <tylerselldesign@gmail.com>
2019-01-10 05:30:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,10 +8,10 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT Projects.title, SUM(pledges.amount)
-FROM projects, pledges
-GROUP BY pledges.project_id, projects.title 
-HAVING projects.id = pledges.project_id 
-ORDER BY projects.title ASC;"
+FROM projects
+JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
